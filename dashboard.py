f7129404510b4245c3f29eda7319353ec376cb78
kushagraaery <66991958+kushagraaery@users.noreply.github.com>
2024-06-25 20:00:01 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import pandas as pd
 import os
 import warnings
+import yaml
 from pandasai import SmartDataframe
 from pandasai.llm import OpenAI
 import plotly.figure_factory as ff
